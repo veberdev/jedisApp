370a5e799ab4c601df2b2b3248e4c6c2ef15f421
veber <weslusisa@gmail.com>
2022-10-26 23:43:37 -0300

    add validations for Municipe model

--- app/models/municipe.rb
+++ app/models/municipe.rb
@@ -1,2 +1,30 @@
 class Municipe < ApplicationRecord
+
+  validates :status, presence: true
+  validates :primeiro_nome, presence: true
+  validates :sobrenome, presence: true
+  validates :cpf, presence: true, uniqueness: true, if: -> { cpf_valid?(cpf) }
+  validates :cns, presence: true, if: -> { cns_valido?(cns) }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w.+-]+@\w+\.\w+\z/, message: "email inválido" }
+  validates :data_nascimento, presence: true, inclusion: { in: (Date.today - 100.years)..(Date.today), message: "data de nascimento inválida"}
+  validates :telefone, presence: true, format: {with: /\A[+]{0,1}[5]{2}[1-9]{2}[9]{1}[0-9]{8}\z/, message: "telefone inválido" }, uniqueness: true
+
+  enum status: {ativo: 0, inativo: 1}
+
+  def cpf_valid?(cpf)
+    puts cpf
+    if CPF.valid?(cpf) == true
+      return true
+    else
+      errors.add(:cpf, message: "cpf inválido")
+    end
+  end
+
+  def cns_valido?(cns)
+    if Cns.new(cns).valido?
+      return true
+    else
+      errors.add(:cns, message: "cns inválido")
+    end
+  end
 end
