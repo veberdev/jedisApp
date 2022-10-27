6ab13e4e058c08d34c31348dd52448c3171ba3d1
veber <weslusisa@gmail.com>
2022-10-27 07:05:39 -0300

    foto validation

--- spec/factories/municipes.rb
+++ spec/factories/municipes.rb
@@ -8,5 +8,12 @@ FactoryBot.define do
     data_nascimento { "01/01/1990" }
     email { Faker::Internet.safe_email }
     telefone { [55419].push(8.times.map{rand(10)}).join }
+    foto {}
   end
+
+  # after(:build) do |municipe|
+  #   file_name = "avatar.png"
+  #   file_path = Rails.root.join("assets", "images", "avatar.png")
+  #   municipe.foto.attach(io: File.open(file_path), filename: file_name, content_type: "image/png")
+  # end
 end
